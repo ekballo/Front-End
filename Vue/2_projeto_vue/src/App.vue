7e9e1c1273fda0c90c6177428007a80b7380c0e5
ekballo <81698678+ekballo@users.noreply.github.com>
2022-01-21 16:43:31 -0300

    LifeCycle

--- Vue/2_projeto_vue/src/App.vue
+++ Vue/2_projeto_vue/src/App.vue
@@ -2,16 +2,20 @@
     <div>
       <PrimeiroComponente />
       <p>O dado de app é {{ testando }}</p>
+      <LifeCycle />
     </div>
 </template>
 
 
 <script>
 import PrimeiroComponente from'./components/PrimeiroComponente.vue'
+import LifeCycle from './components/LifeCycle.vue'
+
   export default{
     nome: 'app',
     components: {
-      PrimeiroComponente
+      PrimeiroComponente,
+      LifeCycle
     },
     data() {
       return {
---
+++ Vue/2_projeto_vue/src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Meu Nome é: {{ nome }} </h1>
+</template>
+
+<script>
+export default {
+    name: 'LifeCycle',
+    data() {
+        return {
+            nome: 'Ainda não sei'
+        }
+    },
+    created() {
+        setTimeout(() =>{
+            this.nome = 'Jefferson'
+        }, 1000);
+    },
+    mounted() {
+        setTimeout(() =>{
+            this.nome = 'Vitoria'
+        }, 2000);
+    }
+}
+</script>
