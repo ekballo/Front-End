b9b76ad222ae41163cf27ef356a18066e21c08ca
ekballo <81698678+ekballo@users.noreply.github.com>
2022-01-25 13:13:33 -0300

    burgerform

---
+++ Vue/projeto_make_burger/src/components/BurgerForm.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <p>Componente de Menssagem</p>
+    </div>
+    <div>
+        <form id="burger-form">
+            <div class="id-container">
+                <label for="nome">Nome do Cliente</label>
+                <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite seu Nome">
+            </div><!--id-container-->
+            <div class="id-container">
+                <label for="pao">Escolha o pão:</label>
+                <select name="pao" id="pao" v-model="pao">
+                    <option value="">Selecione o seu pão</option>
+                    <option value="">Integral</option>
+                </select>
+            </div><!--id-container-->
+        </form><!--burger-form-->
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: "BurgerForm"
+}
+</script>
+
+<style scoped>
+
+</style>
